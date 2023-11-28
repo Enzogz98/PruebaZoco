1e5f4ceba5ddc42aa4b2eeb5d8ca15916db254bf
Enzogz98 <enzo.114998@gmail.com>
2023-11-28 12:45:20 -0300

    cruds completos

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home"
 import './App.css'
+import Libros from "./pages/Libros";
+import Editorial from "./pages/Editorial";
+import Autores from "./pages/Autores";
 
 function App() {
  
@@ -12,6 +15,9 @@ function App() {
     <Routes>
       <Route path="/" element={<Login/>}/>
       <Route path='/home' element={<Home/>} />
+      <Route path='/libros' element={<Libros/>} />
+      <Route path='/editorial' element={<Editorial/>} />
+      <Route path='/autor' element={<Autores/>} />
     </Routes>
     </BrowserRouter>
     </>
